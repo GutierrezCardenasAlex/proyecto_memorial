0e3858d9e97b7e1af73b8071931b47d1feec9ccc
GutierrezCardenasAlex <gutierrezcardenasalex@gmail.com>
2026-03-04 23:42:56 -0400

    update9¿10

--- src/vue/stack/DataManager.vue
+++ src/vue/stack/DataManager.vue
@@ -1,30 +1,20 @@
 <template>
-  <!-- Loader -->
   <Loader
+    v-if="loaderVisible"
     :visible="loaderVisible"
     @completed="onLoaderCompleted"
   />
 
-  <!-- App -->
-  <router-view
-    v-if="appReady"
-    :key="$route.fullPath"
-  />
+  <router-view v-show="appReady" />
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue"
-import { useRoute } from "vue-router"
+import { ref } from "vue"
+// Eliminamos useRoute de aquí, no lo necesitas en este nivel si solo es para la key
 import Loader from "/src/vue/components/loaders/Loader.vue"
-
-const route = useRoute()
 
 const loaderVisible = ref(true)
 const appReady = ref(false)
-
-onMounted(() => {
-  loaderVisible.value = true
-})
 
 function onLoaderCompleted() {
   loaderVisible.value = false
